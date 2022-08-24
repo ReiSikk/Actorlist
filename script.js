9e68e16446bf3f9fc117852c5930a1f98941372f
Rei Sikk <reix0010@stud.kea.dk>
2022-08-24 16:57:34 +0200

    styling and dropdown

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const url = "actors.json";
-const btn = document.querySelector("#moreInfo");
+const btn = document.querySelector(".moreInfo");
 
 fetch(url)
   .then((res) => {
@@ -21,11 +21,27 @@ function showActors(fullname) {
   const copy = template.cloneNode(true);
 
   //change the content
-  copy.querySelector(".actorName").textContent = fullname.fullname;
+  copy.querySelector(".actorName").textContent = `${fullname.fullname}`;
+  copy.querySelector("#movieName").textContent = "- " + `${fullname.movie}`;
 
   //grab the parent
   const parent = document.querySelector("main");
 
   //append the child
   parent.appendChild(copy);
+  document.querySelector(".moreInfo").addEventListener("click", revealInfo);
+}
+
+////////////Drop down function////////
+
+function revealInfo() {
+  const drop = document.querySelector(".dropDown");
+  /*  document.querySelector(".dropDown").classList.remove("hidden");
+  console.log("revealInfo");
+  dropdown.classList.toggle("hidden"); */
+  if (drop.style.display === "none") {
+    drop.style.display = "block";
+  } else {
+    drop.style.display = "none";
+  }
 }
